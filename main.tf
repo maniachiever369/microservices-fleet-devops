5776a2d9b55312582c62740431a07b75ace3795a
maniachiever369 <manikumarl2721@gmail.com>
2026-05-09 23:34:20 +0100

    Initialize Terraform configuration for AWS VPC

---
+++ main.tf
@@ -0,0 +1,25 @@
+# 1. Tell Terraform we want to use AWS
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# 2. Set your region (London is eu-west-2)
+provider "aws" {
+  region = "eu-west-2"
+}
+
+# 3. Create a Virtual Private Cloud (VPC) - Your private 'slice' of the cloud
+resource "aws_vpc" "devops_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "manikanta-project-vpc"
+    Environment = "Dev"
+  }
+}
